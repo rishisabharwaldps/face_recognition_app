ae0a10e18f31fce879cda263f2273b7dc1f62fd4
rishisabharwaldps <75731259+rishisabharwaldps@users.noreply.github.com>
2021-06-11 17:10:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    img_format: 'png',
+    png_quality: 90
+}); 
+
+camera=document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+console.log('ml5 version:', ml5.version);
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+classifier=ml5.imageclassifier('https://teachablemachine.withgoogle.com/models/jMdnWvUpg/model.json', modelLoaded);
+
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+if(error){
+    console.error(error);
+}else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+}
+}
